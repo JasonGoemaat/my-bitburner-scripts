578e73c7e4696a54286cd12f120885aa5d1ebb1e
Jason Goemaat <jason.goemaat@gmail.com>
2022-09-22 12:54:25 -0500

    pull.js - split file names on liens

--- pull.js
+++ pull.js
@@ -11,17 +11,19 @@ const fileNames = `
 
 /** @param {NS} ns */
 export async function main(ns) {
-  
+  let files = fileNames.split('\r\n').filter(x => x.length > 0)
+  if (files.length < 2 && files[0].length === fileNames.length) files = fileNames.split('\n').filter(x => x.length > 0)
+  ns.tprint(`there are ${files.length} files to download...`)
   // await ns.wget(base + 'lib/format.js', 'lib/format.js') // does not work
-  await ns.wget(base + '/lib/format.js', '/lib/format.js')  // this does work
+  // await ns.wget(base + '/lib/format.js', '/lib/format.js')  // this DOES work
 
   //await ns.wget('https://raw.githubusercontent.com/JasonGoemaat/my-bitburner-scripts/main/lib/format.js', 'f1.js')
   // await ns.wget(base + '/lib/format.js', 'f1.js') // WORKS IN ROOT DIRECTORY
   
   // demo, works fine
   // await ns.wget("https://raw.githubusercontent.com/danielyxie/bitburner/master/README.md", "game_readme.txt");
-  while (true) {
-    ns.tprint('waiting 5 seconds...')
-    await ns.sleep(5000)
-  }
+  // while (true) {
+  //   ns.tprint('waiting 5 seconds...')
+  //   await ns.sleep(5000)
+  // }
 }
